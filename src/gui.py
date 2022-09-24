2b6d25bb71e499729e0799562e81632fb6fb73bd
schuyler1007 <kkawata@bu.edu>
2022-09-24 10:42:55 -0400

    add GUI file

---
+++ src/gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class GUI:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Pixel Art GUI")
+        self.root.geometry("1000x1000")
+        self.root.mainloop()        
+
+test = GUI()
